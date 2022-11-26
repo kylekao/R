ba58cf023eaee4ac7df9090851ca0f1df40dd3ff
kylekao <kylekaoster@gmail.com>
2022-11-26 10:40:48 +0800

    Update and rename msmi_exam1.R to msmi_mid_exam1.R

--- msmi_mid_exam1.R
+++ msmi_mid_exam1.R
@@ -6,6 +6,8 @@ str(exam1.df)
 #dollarsspent and satisfaction are all numeric so "2 numeric-> correlation" 
 ggplot(data=exam1.df,aes(x=dollarsspent,y=satisfaction))+geom_point() + geom_smooth(method=lm, se=FALSE)
 corr.test(exam1.df$dollarsspent, exam1.df$satisfaction, method = "pearson", alpha =.05)
-#the plot shows relationship between the amount spent at the museum gift shop and satisfaction with the visit to the museum, it looks like the more dollar spent at the museum gift shop comes with better satisfaction with the visit to the museum.
+#the plot shows relationship between the amount spent at the museum gift shop and satisfaction with the visit to the museum, 
+#it looks like the more dollar spent at the museum gift shop comes with better satisfaction with the visit to the museum.
 
-#then do the correlation test, the probability is 0 means there is no correlation.The estimated correlation is 0.92 suggests a strong, positive association between the amount spent at the museum gift shop and satisfaction with the visit to the museum.
+#then do the correlation test, the probability is 0 means there is no correlation.The estimated correlation is 0.92 suggests a strong,
+#positive association between the amount spent at the museum gift shop and satisfaction with the visit to the museum.
